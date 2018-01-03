7baefa45373b88d374402411dd3564de1c1dd789
robertkarklinsh <robert.karklinsh@stereometria.ru>
2018-01-03 19:46:32 +0300

    For each ts write only last candle to db

--- btfxwss/data/sniffer.py
+++ btfxwss/data/sniffer.py
@@ -7,7 +7,7 @@ from btfxwss.client import BtfxWss
 from sqlalchemy import create_engine
 from btfxwss.data.candle_writer import CandleWriter
 
-logging.basicConfig(filename='test.log', level=logging.DEBUG)
+logging.basicConfig(filename='test.log', level=logging.ERROR)
 log = logging.getLogger(__name__)
 
 
@@ -16,6 +16,7 @@ class Sniffer(QueueProcessor):
         super().__init__(data_q)
         self.candle_handlers = []
         self.timestamp_index = defaultdict(int)
+        self.payload = defaultdict(self._array_factory)
 
     def _handle_candles(self, dtype, data, ts):
         self.log.debug("_handle_candles: %s - %s - %s", dtype, data, ts)
@@ -25,18 +26,27 @@ class Sniffer(QueueProcessor):
         payload = np.array(payload)
         if payload.size == 0:
             return
-        # check for data containing single entity:
+        # check that data is not historical and then write candle when newer one arrives
         if len(payload.shape) != 2:
+            if self.timestamp_index[symbol] == 0:
+                self.timestamp_index[symbol] = payload[0]
+                self.payload[symbol] = payload
+            if self.timestamp_index[symbol] == payload[0]:
+                self.payload[symbol] = payload
             if self.timestamp_index[symbol] < payload[0]:
                 self.timestamp_index[symbol] = payload[0]
-                payload = payload.reshape((1, -1))
-            else:
-                return
-        # formatting payload to [ts, o, h, l, c, v]
-        payload = payload[:, [0, 1, 3, 4, 2, 5]]
+                self.payload[symbol] = self.payload[symbol].reshape((1, -1))
+                # formatting payload to [[ts, o, h, l, c, v]]
+                self.payload[symbol] = self.payload[symbol][:, [0, 1, 3, 4, 2, 5]]
+                for handler in self.candle_handlers:
+                    handler(symbol, self.payload[symbol])
+                self.payload[symbol] = payload
 
-        for handler in self.candle_handlers:
-            handler(symbol, payload)
+        # TODO handle historical candles at the beginning of connection
+
+
+    def _array_factory(self):
+        return np.zeros(6)
 
 
 if __name__ == "__main__":
@@ -48,8 +58,7 @@ if __name__ == "__main__":
     sniffer = Sniffer()
     sniffer.candle_handlers.append(candle_writer.write)
 
-
-    client = BtfxWss(queue_processor=sniffer, log_level=logging.DEBUG)
+    client = BtfxWss(queue_processor=sniffer, log_level=logging.ERROR)
     client.start()
     time.sleep(1)
     client.subscribe_to_candles('BTCUSD')
@@ -65,7 +74,7 @@ if __name__ == "__main__":
     try:
         while True:
             time.sleep(10)
-    #        candle_writer.commit()
+            candle_writer.commit()
     except KeyboardInterrupt as e:
         logging.debug(str(e) + '\n' + 'Exiting...')
         client.stop()
